cd0852df0288aee6e935455856e7e2c280a41245
Muhammad Al Faiz <faiz.burham@gmail.com>
2025-09-08 21:23:53 +0700

    fix custom TextField & Login Page

--- lib/Components/custom_textfield.dart
+++ lib/Components/custom_textfield.dart
@@ -1,9 +1,11 @@
-// lib/components/custom_text_field.dart
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String label;
+  final bool isPassword;
+  final bool isNumber;
+  final Color? borderColor;
   final bool readOnly;
   final VoidCallback? onTap;
   final Widget? suffixIcon;
@@ -12,6 +14,9 @@ class CustomTextField extends StatelessWidget {
     super.key,
     required this.controller,
     required this.label,
+    this.isPassword = false,
+    this.isNumber = false,
+    this.borderColor,
     this.readOnly = false,
     this.onTap,
     this.suffixIcon,
@@ -21,11 +26,17 @@ class CustomTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
+      obscureText: isPassword,
+      keyboardType: isNumber ? TextInputType.number : TextInputType.text,
       readOnly: readOnly,
       onTap: onTap,
       decoration: InputDecoration(
         labelText: label,
-        border: const OutlineInputBorder(),
+        border: OutlineInputBorder(borderSide: BorderSide()),
+        enabledBorder: OutlineInputBorder(borderSide: BorderSide()),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: borderColor ?? Colors.blue, width: 2),
+        ),
         suffixIcon: suffixIcon,
       ),
     );
